075742ba0bb5b5b183972363217f7ca3e3adbb77
Nikita <nikita@sprdnv.com>
2022-06-03 16:22:46 +0200

    fix navbar rendering

--- src/App.js
+++ src/App.js
@@ -40,64 +40,66 @@ function App() {
     <Router>
       <div>
         <nav className='navbar navbar-expand navbar-dark bg-dark'>
-          <Link to={'/'} className='navbar-brand'>
-            Medical
-          </Link>
-          <div className='navbar-nav mr-auto'>
-            <li className='nav-item'>
-              <Link to={'/home'} className='nav-link'>
-                Home
-              </Link>
-            </li>
-            {showAdminBoard && (
+          <div className='container-fluid'>
+            <Link to={'/'} className='navbar-brand'>
+              Medical
+            </Link>
+            <div className='navbar-nav mr-auto'>
               <li className='nav-item'>
-                <Link to={'/admin'} className='nav-link'>
-                  Admin Board
+                <Link to={'/home'} className='nav-link'>
+                  Home
                 </Link>
               </li>
-            )}
-            {showDoctorBoard && (
-              <li className='nav-item'>
-                <Link to={'/doctor'} className='nav-link'>
-                  Doctor Board
-                </Link>
-              </li>
-            )}
-            {currentUser && (
-              <li className='nav-item'>
-                <Link to={'/user'} className='nav-link'>
-                  User
-                </Link>
-              </li>
+              {showAdminBoard && (
+                <li className='nav-item'>
+                  <Link to={'/admin'} className='nav-link'>
+                    Admin Board
+                  </Link>
+                </li>
+              )}
+              {showDoctorBoard && (
+                <li className='nav-item'>
+                  <Link to={'/doctor'} className='nav-link'>
+                    Doctor Board
+                  </Link>
+                </li>
+              )}
+              {currentUser && (
+                <li className='nav-item'>
+                  <Link to={'/user'} className='nav-link'>
+                    User
+                  </Link>
+                </li>
+              )}
+            </div>
+            {currentUser ? (
+              <div className='navbar-nav ml-auto'>
+                <li className='nav-item'>
+                  <Link to={'/profile'} className='nav-link'>
+                    {currentUser.username}
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <a href='/login' className='nav-link' onClick={logOut}>
+                    Log Out
+                  </a>
+                </li>
+              </div>
+            ) : (
+              <div className='navbar-nav ml-auto'>
+                <li className='nav-item'>
+                  <Link to={'/login'} className='nav-link'>
+                    Login
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link to={'/sign-up'} className='nav-link'>
+                    Sign Up
+                  </Link>
+                </li>
+              </div>
             )}
           </div>
-          {currentUser ? (
-            <div className='navbar-nav ml-auto'>
-              <li className='nav-item'>
-                <Link to={'/profile'} className='nav-link'>
-                  {currentUser.username}
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <a href='/login' className='nav-link' onClick={logOut}>
-                  Log Out
-                </a>
-              </li>
-            </div>
-          ) : (
-            <div className='navbar-nav ml-auto'>
-              <li className='nav-item'>
-                <Link to={'/login'} className='nav-link'>
-                  Login
-                </Link>
-              </li>
-              <li className='nav-item'>
-                <Link to={'/sign-up'} className='nav-link'>
-                  Sign Up
-                </Link>
-              </li>
-            </div>
-          )}
         </nav>
         <div className='container mt-3'>
           <Routes>
